6a39a34a7445f94a7ebef4257405b036d34158cb
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-04-20 23:46:32 +0100

    feat: preserve secret values

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,23 @@
 use std::net::TcpListener;
 
+use secrecy::ExposeSecret;
 use sqlx::{Connection, PgPool};
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 use newsletter_app::configuration::get_configuration;
 use newsletter_app::startup::run;
+use newsletter_app::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    LogTracer::init().expect("Failed to set logger");
-
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("newsletter_app".into(), std::io::stdout);
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_subscriber("newsletter_app".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("failed to read configuration");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+    let connection_pool = PgPool::connect(&configuration.database.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
