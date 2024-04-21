a489b8e5d603853d4ef76374756e733516b6a049
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2024-04-22 00:02:01 +0100

    feat: add logic to use ssl mode in database

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,4 @@
-use actix_web::{HttpResponse, Responder, web};
+use actix_web::{web, HttpResponse, Responder};
 use chrono::Utc;
 use serde::Deserialize;
 use sqlx::PgPool;
@@ -20,16 +20,15 @@ subscriber_name = % form.name,
 )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> impl Responder {
-    match insert_subscriber(&form, &pool).await
-    {
+    match insert_subscriber(&form, &pool).await {
         Ok(_) => HttpResponse::Ok().finish(),
-        Err(_) => HttpResponse::InternalServerError().finish()
+        Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
 
 #[tracing::instrument(
-name = "Saving new subscriber details in the database",
-skip(form, pool)
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
 )]
 pub async fn insert_subscriber(form: &FormData, pool: &PgPool) -> Result<(), sqlx::Error> {
     sqlx::query!(
@@ -42,11 +41,11 @@ pub async fn insert_subscriber(form: &FormData, pool: &PgPool) -> Result<(), sql
         form.name,
         Utc::now()
     )
-        .execute(pool)
-        .await
-        .map_err(|e| {
-            tracing::error!("Failed to execute query: {:?}", e);
-            e
-        })?;
+    .execute(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
     Ok(())
 }
